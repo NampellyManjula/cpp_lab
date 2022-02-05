4efa0724d1e9b9821bf07c9381dc8e4631fe11ec
NampellyManjula <85951811+NampellyManjula@users.noreply.github.com>
2022-02-05 13:52:41 +0530

    Add files via upload

---
+++ 14(a) get_put.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char x;
+	cout<<"enter a char:"<<endl;
+	cin.get(x);
+	cout<<"entered char is:"<<endl;
+	cout.put(x);
+	cout.write("\ncongratulations!\n",5);
+	return 0;
+}
